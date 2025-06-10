c6cca9111477b1c3bf80e6ad44a774c141e7628f
safaabalahna <safaabalahna@gmail.com>
2025-06-10 19:24:46 +0100

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,10 +1,12 @@
 def display_menu():
-    print("\nShopping List Manager")
+    print("\n" + "="30)
+    print("Shopping List Manager")
+    print("="30)
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
-
+    print("="*30) 
 def main():
     shopping_list = []
     while True:
