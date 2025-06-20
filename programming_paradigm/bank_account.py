f05c2328f17df24e09493a6a8cf11e2719f3ddf5
safaabalahna <safaabalahna@gmail.com>
2025-06-20 23:01:50 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+    def init(self, account_balance=0):
+        self.account_balance = account_balance
 
     def deposit(self, amount):
         self.account_balance += amount
@@ -13,4 +13,4 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
